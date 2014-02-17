7962b15f683548ae401d43799a00f151382a2278
Jeffrey Chan <jeffreychan637@berkeley.edu>
2014-02-17 01:06:56 -0800

    Added main file

---
+++ Main.java
@@ -0,0 +1,9 @@
+package cpmStats;
+
+/** A program that compiles statistics for CompassPoint Mentorship.
+ *  Information is gathered from CSV files that are made from a
+ *  Google spreadsheet of collected form responses from CPM mentors. */
+public class Main {
+
+
+}
