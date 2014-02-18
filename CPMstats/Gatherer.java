b86bd48b88daf974b348bd1ce8effc7c8173b0fa
Jeffrey Chan <jeffreychan637@berkeley.edu>
2014-02-17 18:18:49 -0800

    Updated file output naming style, Started CSV line reader method, Added test input file

--- CPMstats/Gatherer.java
+++ CPMstats/Gatherer.java
@@ -2,6 +2,10 @@ package CPMstats;
 
 import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Scanner;
+import java.util.HashMap;
 
 /** Gathers all the statistics from the input file and sorts data by
  * Branches and then by Mentors. Also outputs general overall Statistics
@@ -13,9 +17,36 @@ class Gatherer {
 
     /** Creates a Gatherer object that takes data from INPUT, sorts the data,
      *  prints general overall statistics into OUTPUT. */
-    Gatherer(FileReader input, BufferedWriter output) {
+    Gatherer(Scanner input, BufferedWriter output) {
         _input = input;
         _output = output;
+    }
+
+    void sorter() {
+        try {
+        boolean first = true;
+        while (_input.hasNextLine()) {
+            String line = _input.nextLine();
+            if (!first) {
+                String[] lineParts = line.split(",");
+                for (int i = 0; i < lineParts.length; i++) {
+                    System.out.println(lineParts[i]);
+                }
+                System.out.println(lineParts.length);
+            }
+
+
+
+            first = false;
+
+
+        }
+        _input.close();
+        _output.close();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
     }
 
 
@@ -24,8 +55,15 @@ class Gatherer {
 
 
     /** The file that data is read from. */
-    private FileReader _input;
+    private Scanner _input;
     /** The file that statistics will be outputted to. */
     private BufferedWriter _output;
+    /** The total number of mentors who have submitted data THIS MONTH. */
+    private int mentorCount;
+    /** The total number of mentors who used the modules. */
+    private int moduleCount;
+    /** A hashmap of the number of mentors using each module type. */
+    private HashMap<String, Integer> moduleTypeCount;
+
 
 }
--- CPMstats/Main.java
+++ CPMstats/Main.java
@@ -1,13 +1,13 @@
 package CPMstats;
 
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.FileWriter;
 import java.io.BufferedWriter;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Scanner;
 
 /** A program that compiles statistics for CompassPoint Mentorship.
  *  Information is gathered from CSV files that are made from a
@@ -19,7 +19,7 @@ public class Main {
 
     /** Outputs a statistics based on data in ARGS[0] into file ARG[1] if
      *  provided. Otherwise, a new text file is created and named based on
-     *  the current month and year. A usage message is printed if the
+     *  the last month and year. A usage message is printed if the
      *  wrong amount of input arguments is given. */
     public static void main(String[] args) {
         if (args.length > 2 || args.length == 0) {
@@ -27,29 +27,34 @@ public class Main {
             return;
         } else {
             try {
-                FileReader input = new FileReader(new File(args[0]));
+                Scanner input = new Scanner(new File(args[0]));
                 BufferedWriter output = null;
                 if (args.length == 2) {
                     output = new BufferedWriter(new
                                             PrintWriter(new File(args[1])));
                 } else {
                     try {
+                        Calendar cal = Calendar.getInstance();
+                        cal.add(Calendar.MONTH, -1);
+                        if (Calendar.MONTH == 1) {
+                            cal.add(Calendar.YEAR, -1);
+                        }
                         String timeLog = new SimpleDateFormat("MM.yyyy").format(
-                                            Calendar.getInstance().getTime());
+                                            cal.getTime());
                         File outputFile = new
                                 File("TestOutputs/" + timeLog + ".txt");
                                     //"CreatedStats/" + timeLog + ".txt");
                         output = new BufferedWriter(new FileWriter(outputFile));
                         //writer.write("Hello world!");
                     } catch (IOException e) {
-                        System.out.print(e.getMessage());
+                        System.err.println(e.getMessage());
                     }
                 }
                 Gatherer gatherer = new Gatherer(input, output);
                 input.close();
                 output.close();
             } catch (IOException e) {
-                System.out.print(e.getMessage());
+                System.err.println(e.getMessage());
                 System.exit(1);
             }
         }
